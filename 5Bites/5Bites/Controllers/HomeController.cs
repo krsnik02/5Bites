16d807451ed8136d3ff7e197f4cbb933d4a3714c
Garrett Hartshaw <ghartshaw@gmail.com>
2012-12-10 20:57:42 -0500

    Finished transactions page.

--- 5Bites/5Bites/Controllers/HomeController.cs
+++ 5Bites/5Bites/Controllers/HomeController.cs
@@ -21,7 +21,21 @@ namespace _5Bites.Controllers
         [HttpGet]
         public ActionResult Transactions()
         {
-            var m = new _5Bites.Models.Home.Transactions.ViewModel();
+            var m = new Models.Home.Transactions.ViewModel();
+            using (var db = new dbEntities())
+            {
+                var ts = db.Transactions;
+                foreach (var t in ts)
+                {
+                    var tm = new Models.Home.Transactions.TransactionModel();
+                    tm.EmployeeName = t.Employee.Username;
+                    tm.StoreName = t.Store.Location.Name;
+                    tm.ProductName = t.Product.Name;
+                    tm.Quantity = t.Quantity;
+                    tm.Timestamp = t.Timestamp;
+                    m.Transactions.Add(tm);
+                }
+            }
             return View(m);
         }
     }
